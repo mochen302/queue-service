444367e1ecd8ea84fbce41ae6934fc6e7b0b420c
mochen302 <mochen302@163.com>
2019-11-12 11:27:14 +0800

    queue线程模型实现 - v0.01

--- src/queue/queue.go
+++ src/queue/queue.go
@@ -1,54 +1,104 @@
 package queue
 
-import "sync"
+import (
+	"fmt"
+	"github.com/emirpasic/gods/lists/singlylinkedlist"
+	"sync"
+)
 
 type User struct {
 	id       int64
 	nickName int64
+	fmt.Stringer
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("id:%v nickName:%v", u.id, u.nickName)
 }
 
 type QueueStateInfo struct {
 	state   QueueState
 	extInfo string
+	fmt.Stringer
+}
+
+func (stateInfo QueueStateInfo) String() string {
+	return fmt.Sprintf("state:%v extInfo:%v", stateInfo.state, stateInfo.extInfo)
 }
 
 type QueueState int8
 
 const (
-	/*处理成功*/
+	/*处理成功 返回token*/
 	COMPLETE QueueState = 0
-	/*正在排队*/
+	/*正在排队 返回ranking*/
 	ING QueueState = 1
 	/*等待加入队列*/
 	WAIT QueueState = 2
 )
 
-type QueueService struct {
-	queue            chan User
-	maxUserWaitCount int64
-	userMap          map[int64]User
-	lock             *sync.RWMutex
+type UserQueueStateInfo struct {
+	user      User
+	stateInfo QueueStateInfo
+	fmt.Stringer
 }
 
-func New(maxUserCount int64, maxUserWaitCount int64) *QueueService {
+func (userStateInfo UserQueueStateInfo) String() string {
+	return fmt.Sprintf("user:%v stateInfo:%v", userStateInfo.user.String(), userStateInfo.stateInfo.String())
+}
 
+type QueueService struct {
+	handleChan   chan UserQueueStateInfo
+	waitChan     chan UserQueueStateInfo
+	waitList     *singlylinkedlist.List
+	maxWaitCount int
+	userInfoMap  sync.Map
+	lock         *sync.RWMutex
+}
+
+func New(maxHandleCount int, maxWaitCount int) *QueueService {
 	queueService := new(QueueService)
-	queueService.queue = make(chan User, maxUserCount)
-	queueService.userMap = make(map[int64]User)
-	queueService.maxUserWaitCount = maxUserWaitCount
+	queueService.handleChan = make(chan UserQueueStateInfo, maxHandleCount)
+	queueService.waitChan = make(chan UserQueueStateInfo)
+	queueService.maxWaitCount = maxWaitCount
+	queueService.waitList = singlylinkedlist.New()
 	queueService.lock = new(sync.RWMutex)
 
 	return queueService
 }
 
-func (q *QueueService) TryJoin(user User) bool {
-	var queue = q.queue
-	queue <- user
+func (q *QueueService) TryJoin(currentUser User) bool {
+	waitSize := q.waitList.Size()
+	if waitSize >= q.maxWaitCount {
+		Error(currentUser.String(), "try join fail cause the waitSize:", waitSize, ">", q.maxWaitCount)
+		return false
+	}
 
+	id := currentUser.id
+	existUserStateInfo, ok := q.userInfoMap.Load(id)
+	if ok {
+		/**
+		 * 这个地方主要是看具体的业务
+		 */
+		Info(existUserStateInfo.(UserQueueStateInfo).String(), "has join before")
+		return true
+	}
+
+	userStateInfo := UserQueueStateInfo{
+		user: currentUser,
+		stateInfo: QueueStateInfo{
+			state:   WAIT,
+			extInfo: "",
+		},
+	}
+
+	q.userInfoMap.Store(id, userStateInfo)
+	q.waitChan <- userStateInfo
+	Debug(userStateInfo.String(), "join wait queue suc!")
 	return true
 }
 
-func (q *QueueService) QueryState(user User) QueueStateInfo {
-
-	return QueueStateInfo{COMPLETE, "token"}
+func (q *QueueService) QueryState(user User) *UserQueueStateInfo {
+	userStateInfo := new(UserQueueStateInfo)
+	return userStateInfo
 }
